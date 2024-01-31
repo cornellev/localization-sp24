8e4d2dfae902c275a0439ff6c2b91fc136181682
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2024-01-30 20:58:17 -0500

    Create run_utm_silicon.sh

---
+++ run_utm_silicon.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+printf "Press ENTER to confirm you are running this script on Apple Silicon in UTM..."
+read
+printf "Press ENTER to confirm you are in the Lord IMU workspace..."
+read
+printf "Click the USB port button in UTM and select the Lord IMU port. Press ENTER when ready..."
+read
+sudo usermod -a -G dialout $USER
+if [[ -z $(ls /dev | grep ACM0) ]]; then
+  echo "Port ACM0 not found"
+  exit 1
+fi
+sudo chmod a+rw /dev/ttyACM0
+source devel/setup.bash
+roslaunch localization_sp24 localization.launch
