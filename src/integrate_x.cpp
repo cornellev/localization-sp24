9b8c63ea4644d44dbb442f32a4e3f9dcd9590614
Yey007 <yeeeeeeey007@gmail.com>
2024-02-08 14:40:00 -0500

    Idek what's going on anymore but ima merge this

--- src/integrate_x.cpp
+++ src/integrate_x.cpp
@@ -1,30 +1,26 @@
 #include <ros/ros.h>
-#include <std_msgs/String.h>
 #include <sensor_msgs/Imu.h>
+#include <tf2/LinearMath/Vector3.h>
+#include <tf2/LinearMath/Quaternion.h>
 
-double velocity = 0;
-double x = 0;
-ros::Time last_time;
-bool first = true;
+bool started;
+sensor_msgs::Imu removed;
 
 // low pass filter, madgwack, complimentary, ekf, moving averages, exponential
 // moving averages
-void integrate(const sensor_msgs::Imu::ConstPtr& msg) {
-    if (first) {
-        last_time = msg.get()->header.stamp;
-        first = false;
-        return;
-    }
+void remove_gravity(const sensor_msgs::Imu::ConstPtr& msg) {
+    removed = *msg;
 
-    ros::Duration delta_time = msg.get()->header.stamp - last_time;
+    tf2::Quaternion q = tf2::Quaternion(msg->orientation.x, msg->orientation.y,
+        msg->orientation.z, msg->orientation.w);
+    tf2::Vector3 a = tf2::Vector3(msg->linear_acceleration.x,
+        msg->linear_acceleration.y, msg->linear_acceleration.z);
 
-    // https://www.reddit.com/r/gamedev/comments/5wb4hf/have_i_been_doing_second_degree_euler_integration/
-    double accel = msg.get()->linear_acceleration.x;
-    velocity = velocity + accel * delta_time.toSec();
-    x = x + velocity * delta_time.toSec()
-        + 0.5 * accel * delta_time.toSec() * delta_time.toSec();
+    tf2::Vector3 rotated = quatRotate(q, a) - tf2::Vector3(0, 0, 9.81);
 
-    ROS_INFO("%f", x);
+    removed.linear_acceleration.x = rotated.x();
+    removed.linear_acceleration.y = rotated.y();
+    removed.linear_acceleration.z = rotated.z();
 }
 
 int main(int argc, char** argv) {
@@ -34,9 +30,17 @@ int main(int argc, char** argv) {
 
     ROS_INFO("Start");
 
-    ros::Subscriber sub = n.subscribe("/imu/data", 10, integrate);
+    ros::Subscriber sub = n.subscribe("/imu/data", 1, remove_gravity);
+    ros::Publisher pub = n.advertise<sensor_msgs::Imu>("/imu/accel_removed", 1);
 
-    ros::spin();
+    ros::Rate rate = ros::Rate(100);
+
+    while (ros::ok()) {
+        pub.publish(removed);
+
+        ros::spinOnce();
+        rate.sleep();
+    }
 
     return 0;
 }
